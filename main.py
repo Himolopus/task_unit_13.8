2054a698d699c11b4db298f4c3bfa3fc137ab413
unknown <himolopus@gmail.com>
2022-11-05 16:41:02 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+tickets_count = int(input('Сколько билетов желаете приобрести?\n-> '))
+tickets_values = []    # Создаем вспомогательный список в который будут записываться цены заказываемых билетов
+for i in range(tickets_count):    # Это цикл для формирования вспомогательного списка
+    age = int(input(f'Сколько лет {i + 1}-му посетителю?\n-> '))
+    if age < 18:    # Для оптимизации принято решение не дополнять список нулями (для решаемой задачи нам это не нужно)
+        continue
+    value = 1390 if age >= 25 else 990    # К тому же появляется возможность для использования тернарного оператора :)
+    tickets_values.append(value)
+price = sum(tickets_values) if tickets_count <= 3 else int(sum(tickets_values) - (sum(tickets_values) * 10 / 100))
+print(f'Сумма к оплате: {price} руб')
